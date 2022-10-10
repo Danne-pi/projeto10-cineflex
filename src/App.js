7e01beca1a03ee2b6ca45b743e20d42f1e884ea9
Danne <dannepi.dev@gmail.com>
2022-10-10 00:55:30 -0300

    feat: basic session page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import styled, {createGlobalStyle} from "styled-components";
-import Content from "./components/content";
 import Title from "./components/title";
+import AboutFilm from "./components/content/aboutFilm";
+import FilmSelection from "./components/content/filmSelection";
+import FilmSession from "./components/content/filmSession";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 
 export default function App() {
+  const [filmId, setFilmId] = useState(null)
+  const [session, setSession] = useState("--/--/----, --:--")
+
   return (
     <>
     <GlobalStyle />
     <ThisApp>
       <Title />
-      <Content />
+      <>
+      <BrowserRouter>
+        {filmId != null ?
+            <AboutFilm 
+            filmId = {filmId}
+            setFilmId = {setFilmId}
+            />
+        : null}
+            <Routes>
+                <Route path="/" element={<FilmSelection setFilmId = {setFilmId}/>}/>
+                <Route path="/film/:filmId" element={<FilmSession setSession = {setSession} session = {session} />}/>
+            </Routes>
+        </BrowserRouter>
+        </>
     </ThisApp>
     </>
   );
--- src/components/content/filmSession.js
+++ src/components/content/filmSession.js
@@ -1,13 +1,51 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
-export default function FilmSession(){
+export default function FilmSession(props){
     const { filmId } = useParams()
+    const [filmInfo, setFilmInfo] = useState(null);
+
+    useEffect(() => {
+		const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies/"+ filmId +"/showtimes");
+
+		requisicao.then(res => {
+			setFilmInfo(res.data);
+            console.log(res.data);
+		});
+
+		requisicao.catch(err => {
+			console.log(err.response.data);
+		});
+	}, []);
+
+    function DrawDays(){
+        return filmInfo.days.map((item)=> (
+            <div key={item.id}>
+                {item.date}
+            </div>
+        ))
+    }
+
+    if(filmInfo === null) {
+		return <></>;
+	}
+
     return(
         <ThisSession>
             <div className="spacer" />
             <div className="content">
-                {filmId}
+                <div className="film-title">
+                    <img src={filmInfo.posterURL} />
+                    <div>
+                        <h1>{filmInfo.title}</h1>
+                        <h2>{props.session}</h2>
+                    </div>
+                </div>
+                <Body>
+                    <DrawDays />
+                </Body>
             </div>
         </ThisSession>
     );
@@ -17,7 +55,6 @@ const ThisSession = styled.div`
     height: 100%;
     width: 100vw;
     font-family: 'Josefin Sans', sans-serif;
-    font-size: 32px;
 
     .spacer{
         height: 12vh;
@@ -29,9 +66,38 @@ const ThisSession = styled.div`
         background-color: white;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: start;
         align-items: center;
         border-top-right-radius: 25px;
         border-top-left-radius: 25px;
+
+        .film-title{
+            width: 100%;
+            box-sizing: border-box;
+            margin-top: 3vh;
+            gap: 12px;
+            display: flex;
+            padding-inline: 28px;
+            img{
+                object-fit: cover;
+                border-radius: 8px;
+                height: 20vh;
+                width: 33vw;
+            }
+            h1{
+                font-size: 20px;
+            }
+            h2{
+                font-weight: 400;
+            }
+        }
+    }
+`
+const Body = styled.div`
+    margin-top: 4vh;
+
+    div{
+         margin-block: 12px;
+         
     }
 `
